61ade794d689250502c7981e93c04cfb628077a4
aman tyagi <amantyagi7024@gmail.com>
2024-12-14 20:08:51 +0530

    math function

---
+++ script.js
@@ -0,0 +1,40 @@
+let math=Math.PI;
+document.getElementById("demo").innerHTML=math;
+
+let round=Math.round(54.53230);
+document.getElementById("demo1").innerHTML=round;
+
+let ceil=Math.ceil(34.2320);
+document.getElementById("demo2").innerHTML=ceil;
+
+let floor= Math.floor(39.96384);
+document.getElementById("demo3").innerHTML=floor;
+
+let sign=Math.sign(200443);
+document.getElementById("demo4").innerHTML=sign;
+
+let pow=Math.pow(4,2);
+document.getElementById("demo5").innerHTML=pow;
+
+let trunc=Math.trunc(45.9565);
+document.getElementById("demo6").innerHTML=trunc;
+
+let squr=Math.sqrt(12);
+document.getElementById("demo7").innerHTML=squr;
+
+let max =Math.max(23,45,34,23,54,34,34);
+document.getElementById("demo8").innerHTML=max;
+
+
+let min =Math.min(23,45,34,23,54,34,34);
+document.getElementById("demo9").innerHTML=min;
+
+let random=Math.random();
+document.getElementById("demo10").innerHTML=random;
+
+let ten=Math.floor(Math.random()*6);
+document.getElementById("demo11").innerHTML=ten;
+
+let one=Math.floor(Math.random()*10)+1;
+document.getElementById("demo12").innerHTML=one;
+
